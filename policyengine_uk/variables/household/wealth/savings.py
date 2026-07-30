ff4d3971dec727dc8cc9d4ce7c59827d582b165f
Edward McPherson <edward.mcpherson@wpieconomics.com>
2026-07-30 15:12:09 +0100

    Changing savings variable back so that it's defined at a household level, as PE expects.

--- policyengine_uk/variables/household/wealth/savings.py
+++ policyengine_uk/variables/household/wealth/savings.py
@@ -4,21 +4,19 @@ from policyengine_uk.model_api import *
 class savings(Variable):
     label = "Savings"
     documentation = "Household liquid savings"
-    entity = BenUnit
-    definition_period = YEAR
-    value_type = float
-    unit = GBP
-    uprating = "gov.economic_assumptions.indices.obr.per_capita.gdp"
-
-
-'''
-class savings(Variable):
-    label = "Savings"
-    documentation = "Household liquid savings"
     entity = Household
     definition_period = YEAR
     value_type = float
     unit = GBP
     quantity_type = STOCK
     uprating = "gov.economic_assumptions.indices.obr.per_capita.gdp"
-'''
+
+
+class benunit_savings(Variable):
+    label = "Savings"
+    documentation = "Household liquid savings"
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+    uprating = "gov.economic_assumptions.indices.obr.per_capita.gdp"
